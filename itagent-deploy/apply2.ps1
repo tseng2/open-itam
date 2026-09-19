3af49e6460bf382ca7a6ae04fa1a633acdba710f
tseng2 <tseng2@foxmail.com>
2026-09-20 00:11:33 +0800

    chore: remove .zcode, legacy deploy packages and build artifacts from tracking

--- itagent-deploy/apply2.ps1
+++
@@ -1,7 +0,0 @@
-Stop-Process -Name tray -Force -ErrorAction SilentlyContinue
-Stop-Service ITAgentService -Force -ErrorAction SilentlyContinue
-Start-Sleep 2
-Copy-Item 'E:\ai_work\trae\itagent\itagent-deploy\bin\core-agent.exe' 'C:\ProgramData\ITAgent\bin\core-agent.exe' -Force
-Copy-Item 'E:\ai_work\trae\itagent\itagent-deploy\bin\tray.exe' 'C:\ProgramData\ITAgent\bin\tray.exe' -Force
-Start-Service ITAgentService
-"done $(Get-Date)" | Set-Content 'E:\ai_work\trae\itagent\itagent-deploy\apply2.log'
--- itagent-deploy/apply_tray.ps1
+++
@@ -1,4 +0,0 @@
-Stop-Process -Name tray -Force -ErrorAction SilentlyContinue
-Start-Sleep 1
-Copy-Item 'E:\ai_work\trae\itagent\itagent-deploy\bin\tray.exe' 'C:\ProgramData\ITAgent\bin\tray.exe' -Force
-"tray updated $(Get-Date)" | Set-Content 'E:\ai_work\trae\itagent\itagent-deploy\tray_update.log'
